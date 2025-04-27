4474d220b97fc26407c024de3ddeeb56d2b8eb00
zeon256 <me@inve.rs>
2025-04-28 05:27:08 +0800

    feat: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import solidJs from '@astrojs/solid-js';
+import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://doubleprecision.ai',
+  integrations: [mdx(), sitemap(), solidJs()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
